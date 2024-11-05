5cc73094e1a2d3e5eeeb00bb1b2b227557e3e100
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-11-05 21:30:02 +0100

    allow json flag

--- index.ts
+++ index.ts
@@ -3,9 +3,21 @@ import { requestRedstonePayload } from "@redstone-finance/sdk";
 
 const program = new Command();
 
-const makePayload = async (feedId: string, signerCount: number) => {
-  const DATA_SERVICE_ID = "redstone-primary-prod";
+const DATA_SERVICE_ID = "redstone-primary-prod";
 
+const makePayloadStruct = async (feedId: string, signerCount: number) => {
+  const res = await requestRedstonePayload(
+    {
+      dataPackagesIds: [feedId],
+      dataServiceId: DATA_SERVICE_ID,
+      uniqueSignersCount: signerCount,
+    },
+    "json",
+  );
+  console.log(res);
+};
+
+const makePayload = async (feedId: string, signerCount: number) => {
   const res = await requestRedstonePayload(
     {
       dataPackagesIds: [feedId],
@@ -30,12 +42,25 @@ program
   .description("Utility to generate Redstone payload")
   .argument("<feedId>", "feed ID to generate payload for")
   .option("-s, --signers <number>", "number of unique signers", "3")
+  .option("-j, --json", "output JSON payload")
+  .option("-b, --bytes", "output bytes payload")
   .action(async (feedId, options) => {
     try {
       const signerCount = parseInt(options.signers);
-      const payload = await makePayload(feedId, signerCount);
-      const bytes = JSON.stringify(Array.from(payload));
-      console.log(bytes);
+
+      if (!options.json && !options.bytes) {
+        options.bytes = true;
+      }
+
+      if (options.json) {
+        await makePayloadStruct(feedId, signerCount);
+      }
+
+      if (options.bytes) {
+        const payload = await makePayload(feedId, signerCount);
+        const bytes = JSON.stringify(Array.from(payload));
+        console.log(bytes);
+      }
     } catch (err) {
       console.error(err);
       process.exit(1);
